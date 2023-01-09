1e7a4ac91221f90c279c99ef59e08e8dad75ad8e
Azurior <azurior24@gmail.com>
2023-01-09 16:26:13 +0000

    update 17h25

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,66 @@
+let users = [  
+    {  
+        id: 0,  
+        isActive: false,  
+        age: 24,    
+        name: "Marsh Obrien" 
+    },  
+    {  
+        id: 1,  
+        isActive: false,  
+        age: 21,    
+        name: "Rios Gibson" 
+    },  
+    {  
+        id: 2,  
+        isActive: false,  
+        age: 29,  
+        name: "Morgan Buchanan"
+    },  
+    {  
+        id: 3,  
+        isActive: true,  
+        age: 25,    
+        name: "Franklin Dyer" 
+    },  
+    {  
+        id: 4,  
+        isActive: false,  
+        age: 30, 
+        name: "Keller Pitts" 
+    },  
+    {  
+        id: 5,  
+        isActive: false,  
+        age: 25,  
+        name: "Davenport Maddox"  
+    },  
+    {  
+        id: 6,  
+        isActive: true,  
+        age: 31,  
+        name: "Judith Graves"
+    },  
+    {  
+        id: 7,  
+        isActive: true,  
+        age: 26,  
+        name: "Hoffman Hess" 
+    },  
+    {  
+        id: 8,  
+        isActive: true,  
+        age: 22,  
+        name: "Sheena Goff"
+    },  
+    {  
+        id: 9,  
+        isActive: false,  
+        age: 39,  
+        name: "Rose Lawrence" 
+    }  
+];
+
 function menu(){
     
     let h2s = document.querySelectorAll("main > aside h2");
@@ -27,10 +90,20 @@ function menu(){
         
         let main = document.querySelector("body > main > section");
         
+        let span = document.getElementById("span");
+        
         if(aside.classList.contains("open")){
             main.style.gridColumn = "";
         }else{
             main.style.gridColumn = "1 / 3";
+        }
+        
+        if(!header.classList.contains("open")){
+            span.classList.remove("bi-arrow-down-left-square-fill");
+            span.classList.add("bi-arrow-left-square-fill");
+        }else{
+            span.classList.remove("bi-arrow-left-square-fill");
+            span.classList.add("bi-arrow-down-left-square-fill");
         }
     });
 }
